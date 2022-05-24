0b254cf22826f9c6470524922e658aba796297de
Surendra234 <surendramahawar873@gmail.com>
2022-05-24 18:51:21 +0530

    Welcome Message

---
+++ src/com/bridgelabz/Message.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class Message {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Line Comparison computation program");
+    }
+}
